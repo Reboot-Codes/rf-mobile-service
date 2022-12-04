df730a135fea61e54cd3c623a89d8e860e84dddf
Reboot-Codes <git@reboot-codes.com>
2022-12-04 12:01:16 -0700

    Update FAQs.

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import PageLayout from "../components/layout/Page";
 
@@ -8,53 +9,98 @@ const faqs: {
   {
     title: "Do you offer a warranty?",
     response:
-      "Yes! We warranty all repairs for 6 months starting at the date of the invoice covering parts and labor.",
+      "Yes! We offer a 6 month warranty on parts we supplied and labor from the date of the repair.",
   },
   {
     title: "Do you do jump starts?",
-    response: "For sure, just call us over!",
+    response: "For sure, just call us over!*",
   },
   {
     title: "How will I be charged?",
-    response: "You'll be billed per booked hour with a minimum of 1 hour.",
+    response:
+      "We quote our repairs based on the industry book labor rate. Diagnostic charges are separate from the actual repair charge.*",
   },
   {
-    title: "Do you take card?",
-    response: "Yes! We take contactless, card, and cash through Square POS.",
+    title: "What forms of payment do you accept?",
+    response:
+      "We accept most major credit/debit cards, contactless, and U.S. currency.",
   },
   {
     title: "Can you tow my vehicle?",
-    response: "Unfortunately, at this time, we can not tow vehicles.",
+    response:
+      "Unfortunately we do not have the means to provide towing services at this time. However, our ability to bring the shop to you minimizes the need for at tow, but if the circumstances require to have the vehicle relocated we can arrange a tow for you or you are welcome to make the arrangements yourself.",
   },
   {
     title: "Can I pay in installments?",
     response:
-      "While we always suggest paying in full, paying with installments is available through Afterpay.",
+      "While we always suggest paying in full, paying with installments is available through Afterpay.****",
   },
   {
     title: "Where are your services available?",
     response:
-      "We service Tucson, AZ and surrounding areas (e.g. Marana, Oro Valley, Catalina Foothills, Vail, Sahuarita)",
+      "We typically service the Tucson, AZ area, but we are familiar with all of Southern Arizona.**",
   },
   {
     title: "Can I bring my own parts?",
     response:
-      "Unfortunately we cannot use owner provided parts to perform a repair. We want to own the quality of our work and make sure that the correct parts were ordered for a repair.",
+      "We allow our customers to furnish their own parts, but we cannot warranty any parts provided by our customers.***",
   },
   {
-    title: "Can I reschedule an appointment?",
-    response: "We understand that schedules change, ",
+    title: "Can I schedule an appointment?",
+    response:
+      "Scheduling a repair is subject to our workload and availability. We suggest that you contact us for confirmation on that specific time & date.",
+  },
+];
+
+const asterisks: {
+  label: string;
+  response: string | JSX.Element | JSX.Element[];
+}[] = [
+  {
+    label: "*",
+    response: "A minimum 1 hour charge applies to labor.",
+  },
+  {
+    label: "**",
+    response: "Travel charges apply to repairs outside of Tucson, AZ.",
+  },
+  {
+    label: "***",
+    response: "Additional rules apply.",
+  },
+  {
+    label: "****",
+    response: (
+      <>
+        Subject to credit approval by Afterpay.{" "}
+        <Link
+          href="https://www.afterpay.com/terms-of-service"
+          target="_blank"
+          className="underline"
+        >
+          Additional terms apply.
+        </Link>
+      </>
+    ),
   },
 ];
 
 export default function Page() {
   return (
     <PageLayout name="FAQ">
-      <div className="w-[90%] mx-auto px-3 grid gap-3 md:grid-cols-2 lg:grid-cols-3 mt-5">
+      <div className="w-[90%] mx-auto px-3 grid gap-3 md:grid-cols-2 mt-5 mb-10">
         {faqs.map((faq, id) => (
           <div key={id} className="max-w-md">
             <h2 className="text-2xl font-bold">{faq.title}</h2>
             <p className="md:text-lg">{faq.response}</p>
+          </div>
+        ))}
+      </div>
+      <div className="w-[90%] mx-auto text-sm">
+        {asterisks.map((asterisk, id) => (
+          <div key={id} className="mb-2 flex flex-row space-x-1 mx-3">
+            <p>{asterisk.label}</p>
+            <p>{asterisk.response}</p>
           </div>
         ))}
       </div>
